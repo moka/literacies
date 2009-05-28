004168729ca841eba7b276659e187305b763bfd5
moka <moka@cerego.co.jp>
2009-05-28 21:18:08 +0900

    update

--- c/argv_002_pointer_and_address_2.c
+++ c/argv_002_pointer_and_address_2.c
@@ -15,11 +15,25 @@ int main(int argc, char **argv){
 	printf("%d\n",**argv+1);
 	printf("%s\n//","printf(\"%c\",*argv+1);");
 	printf("%c\n",**argv+1);
-
+//
 	char c = **argv;
-	printf("%c",c);
+	printf("%s\n","char c = **argv;");
+	printf("%s\n//","printf(\"%c\",c);");
+	printf("%c\n",c);
+	printf("%s\n//","printf(\"%c\",&c);");
+	printf("%c\n",&c);
+
+	printf("%s\n//","printf(\"%d\",&\"abcdefg\");");
+	printf("%d\n",&"abcdefg");
+	printf("%s\n//","printf(\"%d\",\"abcdefg\");");
+	printf("%d\n","abcdefg");
 	printf("\n");
-	printf("%c",&c);
-	printf("\n");
-		
+	char *b = "abcdefg";
+	printf("%s\n","char *b = \"abcdefg\";");
+	printf("%s\n//","printf(\"%d\",b);");
+	printf("%d\n",b);
+	printf("%s\n//","printf(\"%s\",b);");
+	printf("%s\n",b);
+	printf("%s\n//","printf(\"%c\",b);");
+	printf("%c\n",b);
 }
